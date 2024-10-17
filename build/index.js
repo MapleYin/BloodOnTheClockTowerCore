d4e5d7146c3f33c9b0ed37a2ae6e747335e28e59
Yin Heng <maple1058@hotmail.com>
2024-10-17 14:01:21 +0800

    add

--- build/index.js
+++ build/index.js
@@ -725,10 +725,16 @@ var Slay = {
   key: "Slay",
   validate: () => true,
   effect: (operation, players) => {
+    if (!operation.payload) {
+      return;
+    }
     const effectorPlayer = players[operation.effector];
     const player = players[operation.payload?.target];
     if (effectorPlayer.character.abilities.includes("Slay") && hasRealAbility(effectorPlayer) && player && player.character.kind === "Demons") {
       player.isSlew = true;
+      operation.payload.result = true;
+    } else {
+      operation.payload.result = false;
     }
     if (effectorPlayer.character.abilities.includes("Slay")) {
       effectorPlayer.character.abilities.splice(effectorPlayer.character.abilities.indexOf("Slay"), 1);
