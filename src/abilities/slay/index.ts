a0a0e0413d6b37e81d55b583073372911f09bbea
Yin Heng <maple1058@hotmail.com>
2024-10-18 12:58:15 +0800

    add

--- src/abilities/slay/index.ts
+++ src/abilities/slay/index.ts
@@ -4,13 +4,25 @@ export const Slay: BCT.TAbility = {
     key: "Slay",
     validate: () => true,
     effect: (operation, players) => {
+        const player = players[operation.payload?.target]
+        if (!player) {
+            return
+        }
+        player.isSlew = true
+    },
+    effecting: (operation, players, timelines) => {
         const effectorPlayer = players[operation.effector]
         const player = players[operation.payload?.target]
-        if (effectorPlayer.character.abilities.includes("Slay") && hasRealAbility(effectorPlayer) && player && player.character.kind === "Demons") {
-            player.isSlew = true
+        if (!hasRealAbility(effectorPlayer) || !effectorPlayer.character.abilities.includes("Slay") || !player || player.character.kind !== "Demons") {
+            return false
         }
-        if (effectorPlayer.character.abilities.includes("Slay")) {
-            effectorPlayer.character.abilities.splice(effectorPlayer.character.abilities.indexOf("Slay"), 1)
+        if (timelines.findIndex(t => {
+            return t.operations.findIndex(op => {
+                return op != operation && op.abilityKey === "Slay" && op.effector === operation.effector
+            }) != -1
+        }) != -1) {
+            return false
         }
+        return true
     }
 }
